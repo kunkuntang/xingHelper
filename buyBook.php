61276e7778dd22a0f13d5a330f6076bb814dd0c3
kunkuntang <kuntang@163.com>
2016-02-23 21:47:09 +0800

    还差管理员登录了

--- buyBook.php
+++ buyBook.php
@@ -25,6 +25,7 @@
 		<title></title>
 		<link rel="stylesheet" type="text/css" href="css/reset.css" />
 		<link rel="stylesheet" type="text/css" href="css/buyBook.css" />
+		<script src="js/zepto.min.js"></script>
 	</head>
 
 	<body>
@@ -38,10 +39,11 @@
 			<div class="bookListName">大二上学年书单</div>
 			<div class="stuInfo">
 				<span class="stuName">名字：</span>
-				<input type="text" name="stuName" />
+				<input type="text" name="stuName" onblur="onBlur($(this))" id="stuName" />
 				<span class="stuNum">学号：</span>
-				<input type="text" name="stuNum" />
+				<input type="text" name="stuNum" onblur="onBlur($(this))" id="stuNum" />
 				<div class="btn small buy" id="buy" onclick="javascript:document.getElementById('buyList').submit();">确定购买</div>
+				<div class="btn small buy" id="buy" onclick="checkBook()">查询</div>
 
 			</div>
 		</div>
@@ -55,13 +57,66 @@
 					<span class="price">价格:<?php echo $row['bookprice'];?>元</span>
 					<span class="discount">折扣:<?php echo $row['discount'];?>折</span>
 					<span class="disPrice">折后单价:<?php echo $row['bookprice']*$row['discount'];?>元</span>
-					<input type="checkbox" name="<?php echo $row['id'];?>" />
+					<input type="checkbox" name="<?php echo $row['id'];?>" id="<?php echo $row['id'];?>" />
 					<label for="buyBook">购买</label>
 				</div>
 				<input type="text" name="booklistId" value="<?php echo $id;?>" style="display:none" />
 			<?php endforeach;?>
+				<input type="text" id="username" name="username" style="display:none" />
+				<input type="text" id="usernum" name="usernum" style="display:none" />
 			</form>
 		</div>
 	</body>
+	
+	<script type="text/javascript">
+		var arr = ['12','11'];
+		console.log($("#12"));
+		console.log($("#"+arr[0]));
+		//$('#12').attr("checked","check");
+		function onBlur($this){
+			var value = $this[0].value;
+			if($this[0].name == "stuName"){
+				$('#username').val(value);
+			}
+			if($this[0].name == "stuNum"){
+				$('#usernum')[0].value=value;
+			}
+		}
+		
+		function checkBook(){
+			var uNum = $("#stuNum")[0].value;
+			//console.log($(".book input")[0]);
+			var len = $(".book input").length;
+			//$(".book input")[0].checked = "checked";
+			console.log(uNum)
+			$.ajax({
+				type: 'post',
+				url: 'doAction.php',
+				data: {act: 'checkBook',usernum: uNum},
+				dataType: 'json',
+				success: function(data){
+					console.log('success');
+					console.log(data.length);
+					//console.log($("#"+data));
+					var username = data[0];
+					data.shift();
+					$("#stuName")[0].value = username;
+					for(var j = 0; j < len; j++){
+						$(".book input")[j].checked = "";
+					}
+					for(var i = 0; i < data.length; i++){
+						$("#"+data[i])[0].checked = "checked";
+					}
+				},
+				error: function(XMLHttpRequest, textStatus, errorThrown){
+					console.log(XMLHttpRequest.status);
+                    console.log(XMLHttpRequest.readyState);
+                    console.log(textStatus);
+					alert('没有此用户信息！！');
+				}
+			});
+		}
+		
+	</script>
 
 </html>
--- doAction.php
+++ doAction.php
@@ -57,26 +57,57 @@ if($act == 'publish'){
 	}
 	mysql_query("END");
 	
-}elseif($act == 'edit'){
-	
+}elseif($act == 'edit'){	
 	updateListName($id,$arr);
-
-	
 }elseif($act == 'buy'){
 	$booklistId = $arr['booklistId'];
+	$username = $arr['username'];
+	$usernum = $arr['usernum'];
+	$sql = "delete from userlist where usernum = {$usernum}";
+	mysql_query($sql);
+	print_r($arr);
 	unset($arr['act']);
 	unset($arr['booklistId']);
+	unset($arr['username']);
+	unset($arr['usernum']);
 	foreach ($arr as $key => $val) {
+		mysql_query("BEGIN");
 		$sql = "select buynum from booklist where id={$key}";
 		$buyNum = fetchOne($sql);	
 		$buyNum = $buyNum['buynum'] + 1;		
 		$sql = "update booklist set buyNum = '{$buyNum}' where id={$key}";
-		mysql_query($sql);
-		$result = mysql_affected_rows();
-	}
+		$result1 = mysql_query($sql);
+		$sql = "insert userlist (username,usernum,bookid) values ('{$username}','{$usernum}','{$key}')";
+		$result2 = mysql_query($sql);
+		if($result2 && $result1){
+			mysql_query("COMMIT");
+			$result = true;
+		}else{
+			mysql_query("ROLLBACK");
+			$result = false;
+		}
+	}   
 	if($result){
 		alertMes("购买成功！！","buybook.php?booklist={$booklistId}");
 	}else{
 		alertMes("购买失败！！","buybook.php?booklist={$booklistId}");
 	}
+}elseif($act == "checkBook"){
+	//print_r($arr);
+	$usernum = $arr['usernum'];
+	$result = array();
+	$sql = "select username,bookId from userlist where usernum = {$usernum}";
+	//echo $sql;
+	$rows = fetchAll($sql);
+	//print_r($rows);
+	$isName = false;
+	foreach ($rows as $row) {
+		if(!$isName){
+			array_push($result, $row['username']);
+			$isName = true;
+		}
+		array_push($result, $row['bookId']);
+	}
+	echo json_encode($result);
+	exit();
 }
